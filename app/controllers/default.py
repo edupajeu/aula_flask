3eb8c9fafc9e2c0677f3bcac3775750019476239
edupajeu <edupajeu@gmail.com>
2018-10-13 16:56:16 -0300

    Ajustes nas rotas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,16 @@
 from app import app
 
 
+@app.route("/index")
 @app.route("/")
 def index():
+    return "Apresenta o index"
+
+
+@app.route("/test/<int:id>")
+def test(id):
     return "Teste"
+
+@app.route("/test1/", methods=['GET'])
+def test1():
+    return "Teste1"
