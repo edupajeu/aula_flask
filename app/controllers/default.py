16025975281868209cdd2b2bcd2e9c46d1764186
edupajeu <eduardo@gtek.com.br>
2018-10-17 18:08:37 -0300

    Inclusão da página de login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,17 +1,23 @@
 from flask import  render_template
 from app import app
 
+from app.models.forms import LoginForm
 
-@app.route("/index/<user>")
-@app.route("/", defaults={"user":None})
-def index(user):
-    return render_template('index.html', user=user
-                           )
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 
-@app.route("/test/<int:id>")
-def test(id):
-    return "Teste"
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    return render_template('login.html',
+                           form=form)
+
 
 @app.route("/test1/", methods=['GET'])
 def test1():
